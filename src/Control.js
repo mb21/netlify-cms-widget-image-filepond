8a72201b8f3dc1f6c87f41cc4af07ec3df621443
mb21 <mb21@users.noreply.github.com>
2019-09-16 16:59:00 +0200

    initial changes to Control.js

--- src/Control.js
+++ src/Control.js
@@ -1,34 +1,86 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import { FilePond, registerPlugin } from 'react-filepond';
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+
+import 'filepond/dist/filepond.min.css';
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+
+//import { createAssetProxy } from 'netlify-cms-core/src/ValueObjects/AssetProxy';
+//import { NetlifyCmsCore as CMS } from 'netlify-cms-core';
+
+
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
 export default class Control extends React.Component {
   static propTypes = {
-    onChange: PropTypes.func.isRequired,
-    forID: PropTypes.string,
-    value: PropTypes.node,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
     classNameWrapper: PropTypes.string.isRequired,
-  }
+    addAsset: PropTypes.func.isRequired,
+    getAsset: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+  };
 
   static defaultProps = {
     value: '',
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // Set initial files, type 'local' means this is a file
+      // that has already been uploaded to the server (see docs)
+      files: [
+        {
+          source: "index.html",
+          options: {
+            type: "local"
+          }
+        }
+      ]
+    };
+  }
+
+  handleSubmit = e => {
+    const assetProxy = '' //await createAssetProxy(fileName, file);
+    this.props.onAddAsset(assetProxy);
   }
 
   render() {
     const {
-      forID,
       value,
-      onChange,
       classNameWrapper,
+      onAddAsset,
+      getAsset,
+      onChange,
     } = this.props;
 
     return (
-      <input
-        type="text"
-        id={forID}
-        className={classNameWrapper}
-        value={value || ''}
-        onChange={e => onChange(e.target.value)}
-      />
+      <div className={classNameWrapper}>
+      { value
+        ? <img src={getAsset(value)} />
+        : <div>
+            <FilePond
+              ref={ref => (this.pond = ref)}
+              files={this.state.files}
+              allowMultiple={false}
+              maxFiles={3}
+              //server="/api"
+              //oninit={() => this.handleInit()}
+              onupdatefiles={fileItems => {
+                console.log(fileItems);
+                this.setState({
+                  files: fileItems.map(fileItem => fileItem.file)
+                });
+              }}
+              />
+              <button onClick={this.handleSubmit.bind(null)}>Submit</button>
+            </div>
+      }
+      </div>
     );
   }
 }
